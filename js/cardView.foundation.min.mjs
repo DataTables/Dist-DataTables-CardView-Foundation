aa01dda7d185387f267d0b0bb330fa5a8a4ca3aa
dtbuild <dtbuild@rigil>
2026-07-22 12:55:40 +0000

    1321869da9b93343fb1fcc2bfb194c51b091cc96 Release 1.0.0!
    Sync to source repo @1321869da9b93343fb1fcc2bfb194c51b091cc96

--- js/cardView.foundation.min.mjs
+++ js/cardView.foundation.min.mjs
@@ -1,4 +1,4 @@
-/*! CardView Foundation styling 1.0.0-dev for DataTables
+/*! CardView Foundation styling 1.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 import DataTable from"datatables.net-zf";import CardView from"datatables.net-cardview";Object.assign(DataTable.CardView.classes,{card:"card dtcv-card",cardContent:"card-section"});export default DataTable;
