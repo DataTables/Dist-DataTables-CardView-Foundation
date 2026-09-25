2a15692a3426eba0cddc96e7d3ff1f24a49f3614
dtbuild <dtbuild@rigil>
2026-09-25 11:04:03 +0000

    5bc8d4ed681a29a34ae2ee9578eedc247fba00a3 Release 1.1.2
    Sync to source repo @5bc8d4ed681a29a34ae2ee9578eedc247fba00a3

--- js/cardView.foundation.js
+++ js/cardView.foundation.js
@@ -1,4 +1,4 @@
-/*! CardView Foundation styling 1.1.1 for DataTables
+/*! CardView Foundation styling 1.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 
